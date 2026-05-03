54c9a20e0a276d4b1022df295fa13b4b5e681163
bizhab <smpaisyiyah01@gmail.com>
2026-05-03 23:14:14 +0800

    fix: selesaikan konflik merge pada routes web.php

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
-<<<<<<< HEAD
 
-// Fitur READ [GET] - Menampilkan daftar task
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-
-// Fitur DELETE [DELETE] - Menghapus task
-Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-=======
->>>>>>> 3438bc0192a4dd0a6915c5aaf1674e6eba78db2f
-
+// ==========================================
+// Halaman Utama
+// ==========================================
 Route::get('/', function () {
     return view('welcome');
 });
 
-
+// ==========================================
+// Rute untuk Fitur CREATE (Dikerjakan Temanmu)
+// ==========================================
 Route::post('/tasks', [TaskController::class, 'store']);
 
+// ==========================================
+// Rute untuk Fitur READ & DELETE (Dikerjakan Olehmu)
+// ==========================================
+
+// Menampilkan daftar task
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+
+// Menghapus task
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
